13c69f2da2d20ffd807a45b57298ffbbd8292ed8
StevenUninorte <stevenforgir@gmail.com>
2025-12-17 17:21:44 -0500

    update notification status

--- src/main/java/com/steven_udemy/notification_system/model/NotificationEvent.java
+++ src/main/java/com/steven_udemy/notification_system/model/NotificationEvent.java
@@ -1,14 +1,18 @@
 package com.steven_udemy.notification_system.model;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 
 @Slf4j
+@Data
 public class NotificationEvent {
 
+    private String id;
     private String source;
     private String message;
     private Priority priority;
     private LocalDateTime timestamp;
+    private NotificationStatus status;
 }
